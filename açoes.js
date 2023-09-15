634e43b3f3efc37cd02d6367f50dcc9e231de755
ViniciusFelipeLobo <123911832+ViniciusFelipeLobo@users.noreply.github.com>
2023-09-15 10:40:38 -0300

    Atualização

--- açoes.js
+++ açoes.js
@@ -2,6 +2,7 @@ lucide.createIcons();
 const linkCss = document.querySelector('#css');
 const botaoDarkLight = document.querySelector('.material-symbols-outlined');
 botaoDarkLight.addEventListener('click', ()=>{
+    botaoDarkLight.setAttribute('id', 'rotate')
     if(linkCss.getAttribute('href') ==='style.css'){
         linkCss.setAttribute('href', 'styledark.css')
     }else{
