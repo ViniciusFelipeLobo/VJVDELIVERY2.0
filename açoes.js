efa410d4fab8d558761375f160585289def00a59
ViniciusFelipeLobo <123911832+ViniciusFelipeLobo@users.noreply.github.com>
2023-09-18 13:51:28 -0300

    Atualização

--- açoes.js
+++ açoes.js
@@ -1,11 +1,25 @@
+//importando os icones do desing.
 lucide.createIcons();
+//trocando a pagina de light para dark e também operando com uma animação.
 const linkCss = document.querySelector('#css');
 const botaoDarkLight = document.querySelector('.material-symbols-outlined');
 botaoDarkLight.addEventListener('click', ()=>{
-    botaoDarkLight.setAttribute('id', 'rotate')
+    botaoDarkLight.setAttribute('id', 'rotate');
     if(linkCss.getAttribute('href') ==='style.css'){
-        linkCss.setAttribute('href', 'styledark.css')
+        linkCss.setAttribute('href', 'styledark.css');
     }else{
-        linkCss.setAttribute('href', 'style.css')
+        linkCss.setAttribute('href', 'style.css');
     }
+})
+//Colocando a lista de maneira reponsiva para dispositivos Móveis.
+const nav = document.querySelector('nav');
+const menu = document.querySelector('#menu');
+const lista = document.querySelector('ul')
+const larguraDaTela = window.innerWidth;
+const section = document.querySelector('section');
+window.addEventListener('resize', ()=>{
+if(larguraDaTela <= 700){
+nav.removeChild(lista);
+section.appendChild(lista);
+}
 })
