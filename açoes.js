cfa4216556503a156674f6967c84eb9326ae923f
ViniciusFelipeLobo <123911832+ViniciusFelipeLobo@users.noreply.github.com>
2023-09-20 21:06:13 -0300

    Atualização

--- açoes.js
+++ açoes.js
@@ -18,7 +18,7 @@ const lista = document.querySelector('ul')
 const larguraDaTela = window.innerWidth;
 const section = document.querySelector('section');
 const btnMenu = document.querySelector('#btnMenu');
-btnMenu.addEventListener('click', ()=>{
+btnMenu.addEventListener('touchstart', ()=>{
     btnMenu.classList.toggle("btnClicar")
     if(menu.style.display == 'none'){
         menu.style.display = 'block';
