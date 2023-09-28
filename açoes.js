3e1a141fce8e7241b4fb7bb88d3d864dad116d57
ViniciusFelipeLobo <123911832+ViniciusFelipeLobo@users.noreply.github.com>
2023-09-28 13:17:52 -0300

    atualização

--- açoes.js
+++ açoes.js
@@ -19,21 +19,15 @@ const larguraDaTela = window.innerWidth;
 const section = document.querySelector('section');
 const botoes = document.getElementsByTagName('button');
 botoes[0].addEventListener('click', ()=>{
-    //verificando se está no modo light ou dark para fazer a troca de coloração dos botões
-    if(linkCss.getAttribute('href') === 'style.css'){
-        botoes[0].style.backgroundColor = '#7f7f80';
-    }else{
-        botoes[0].style.backgroundColor = '#ffffff';
-        // Aninhamento para fazer a troca de coloração do texto no botão quando voltar ao inicio 
-        if(botoes[0].style.backgroundColor == '#ffffff'){
-            botoes[0].style.color = '#8257e5';
-        }else{
-            botoes[0].style.color = '#ffffff';
-        }
-    }
     // Quando o evento de clique for acionado mostrar o menu 
     if(menu.style.display == 'none'){
         menu.style.display = 'block';
+        if(linkCss.getAttribute('href') === 'style.css'){
+            botoes[0].style.backgroundColor = '#7f7f80';
+        }else{
+            botoes[0].style.backgroundColor = '#ffffff';
+            botoes[0].style.color = '#8257e5';
+        }
         // Aqui se a largura da tela estiver para dispositivos movéis vai fazer uma alteração de desing
         if(larguraDaTela <= 700){
         nav.removeChild(lista);
@@ -42,8 +36,12 @@ botoes[0].addEventListener('click', ()=>{
     }
     }else{
         menu.style.display = 'none';
-        botoes[0].style.backgroundColor = '#8257e5';
-
+        if(linkCss.getAttribute('href') === 'style.css'){
+            botoes[0].style.backgroundColor = '#8257e5';
+        }else{
+            botoes[0].style.backgroundColor = '#8257e5';
+            botoes[0].style.color = '#ffffff'
+        }
     }
 
 })
